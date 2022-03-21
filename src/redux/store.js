0413726a70b06625ed4f2e9cb6cbe9ccefeed6f3
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-03-21 23:26:54 +0200

    configured redux store

---
+++ src/redux/store.js
@@ -0,0 +1,40 @@
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import storage from 'redux-persist/lib/storage';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import authReducer from './auth/auth-slice';
+// import contactsReducer from './contacts/contacts-slice';
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+const authPersistReducer = persistReducer(authPersistConfig, authReducer);
+
+const rootReducer = combineReducers({
+  auth: authPersistReducer,
+  //   contacts: contactsReducer,
+});
+
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+  devtools: process.env.NODE_ENV === 'development',
+});
+
+export const persistor = persistStore(store);
