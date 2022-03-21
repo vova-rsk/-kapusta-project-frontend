0076d21109ffebb8f01bf21234c8e0653ef9882e
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-03-21 15:36:35 +0200

    reworked component

--- src/components/LogoutPanel/LogoutPanel.jsx
+++ src/components/LogoutPanel/LogoutPanel.jsx
@@ -1,48 +1,33 @@
-import PropTypes from 'prop-types';
-import { Container, StyledAvatar, SlytedButton } from './LogoutPanel.styled';
-import SvgIcon from '@mui/material/SvgIcon';
-import { ReactComponent as ExitLogoSvg } from './exit-logo.svg';
-import IconButton from '@mui/material/IconButton';
+import useWindowDimensions from '../../hooks/useWindowDimensions';
+import {
+  Container,
+  StyledAvatar,
+  PersonalInfoContainer,
+} from './LogoutPanel.styled';
+import LogoutButton from '../LogoutButton';
 
-function LogoutPanel(props) {
-  const auth = false;
+function LogoutPanel() {
+  const { width } = useWindowDimensions();
+  const auth = true;
+
   return (
     <Container>
-      <div>
+      <PersonalInfoContainer>
         <StyledAvatar
           sx={{
             width: '32px',
             height: '32px',
           }}
         />
-        {auth && <span>User name</span>}
-      </div>
-      <div>
-        {/* <SlytedButton sx={{width:'32px', height:'32px'}}>
-                    <SvgIcon
-                        component={ExitLogoSvg}
-                        sx={{ width: '16px', height: '16px' }}
-                        inheritViewBox={true}
-                    />
-                        
-              
-                    /* <span>Выход</span>
-            </SlytedButton> */}
-        <IconButton
-          component={ExitLogoSvg}
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            alignItems: 'center',
-            width: '32px',
-            height: '32px',
-          }}
-        ></IconButton>
-      </div>
+        {auth && width >= 768 && <span>User name</span>}
+      </PersonalInfoContainer>
+      <LogoutButton
+        handleClick={() => {
+          alert('handleClick for logout!');
+        }}
+      />
     </Container>
   );
 }
-
-LogoutPanel.propTypes = {};
 
 export default LogoutPanel;
--- src/components/LogoutPanel/LogoutPanel.styled.jsx
+++ src/components/LogoutPanel/LogoutPanel.styled.jsx
@@ -1,23 +1,33 @@
 import styled from '@emotion/styled';
 import Avatar from '@mui/material/Avatar';
-import Button from '@mui/material/Button';
 
 export const Container = styled.div`
   display: flex;
+  flex-wrap: nowrap;
   align-items: center;
 `;
 
-export const StyledAvatar = styled(Avatar)``;
+export const PersonalInfoContainer = styled.div`
+  display: flex;
+  flex-wrap: nowrap;
+  align-items: center;
+  padding-right: 20px;
+  border-right: 1px solid #e0e5eb;
 
-export const SlytedButton = styled(Button)`
-  // padding:0;
-  width: 32px;
-  height: 32px;
-  border-radius: 50%;
+  @media screen and (max-width: 767px) {
+    margin-right: 7px;
+    padding-right: 0;
+    border-right: none;
+  }
 
-  // @media screen and (max-width:767px){
-  //     display:flex;
-  //     justify-content:center;
-  //     align-items:center;
-  // }
+  & span {
+    margin-left: 10px;
+    font-weight: 400;
+    font-size: 12px;
+    line-height: 1.167;
+    letter-spacing: 0.04em;
+    color: #52555f;
+  }
 `;
+
+export const StyledAvatar = styled(Avatar)``;
