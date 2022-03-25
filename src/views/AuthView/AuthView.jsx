e501f72b5b81efba31922654dddfb36656c9f8d4
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-03-25 21:45:21 +0200

    fix

--- src/views/AuthView/AuthView.jsx
+++ src/views/AuthView/AuthView.jsx
@@ -1,8 +1,24 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate, useLocation } from 'react-router-dom';
 import { Container, TitleContainer } from './AuthView.styled';
+import { getAuthStatus } from '../../redux/auth/auth-selectors';
 import Title from '../../components/Title';
 import AuthForm from '../../components/Forms/AuthForm';
 
-function authView() {
+function AuthView() {
+  const navigate = useNavigate();
+  const location = useLocation();
+  const auth = useSelector(getAuthStatus);
+
+  useEffect(() => {
+    const { from } = location.state;
+
+    if (auth) {
+      navigate(`../${from.pathname}` || '../finance');
+    }
+  }, [auth, location, navigate]);
+
   return (
     <Container className="authContainer">
       <TitleContainer>
@@ -13,4 +29,4 @@ function authView() {
   );
 }
 
-export default authView;
+export default AuthView;
