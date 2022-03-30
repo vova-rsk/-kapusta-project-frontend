44c30ce6573b1814ceb06945198e799fb7c6deed
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-03-30 22:50:59 +0300

    fix

--- src/components/NavigationControl/NavigationControl.jsx
+++ src/components/NavigationControl/NavigationControl.jsx
@@ -9,11 +9,11 @@ function NavigationControl() {
   const location = useLocation();
   const { width } = useWindowDimensions();
 
-  const isReportsView = !location.pathname.includes('/reports');
+  const isReportsView = location.pathname.includes('/reports');
+  const isHideTextLabel = width < 768 && isReportsView;
   const textLabel = !isReportsView
     ? 'Перейти к отчетам'
     : 'Вернуться на главную';
-  const isHideTextLabel = width < 768 && isReportsView;
 
   return (
     <StyledNavLink to={isReportsView ? '/finance' : '/reports'}>
