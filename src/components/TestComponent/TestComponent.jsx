99378a0b8e4040d59b421a29ed4d9152f3633bc1
Volodymyr <77401737+vova-rsk@users.noreply.github.com>
2022-03-30 14:35:12 +0300

    update

--- src/components/TestComponent/TestComponent.jsx
+++ src/components/TestComponent/TestComponent.jsx
@@ -4,6 +4,10 @@ import { useNavigate } from 'react-router-dom';
 import * as categoriesOperations from '../../redux/categories/categories-operations';
 import * as balanceOperations from '../../redux/balance/balance-operations';
 
+import BalanceForm from '../Forms/BalanceForm';
+import NavigationControl from '../NavigationControl';
+import TimelapseControl from '../TimelapseControl';
+
 function TestComponent() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -19,6 +23,9 @@ function TestComponent() {
       <button onClick={() => navigate('/finance/income')}>INCOME</button>
       <button onClick={() => navigate('/finance/cost')}>COST</button>
       <button onClick={() => navigate('/reports')}>REPORTS</button>
+      {/* <BalanceForm /> */}
+      <NavigationControl />
+      <TimelapseControl />
     </div>
   );
 }
